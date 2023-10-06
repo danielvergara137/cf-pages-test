db9a03bb57ba5f3ee9b580477f9cb1820b34604c
danielvergara137 <danielvr137@gmail.com>
2023-10-06 10:10:18 -0300

    first commit

---
+++ functions/contactform.js
@@ -0,0 +1,37 @@
+const SECRET_KEY = '0x4AAAAAAAKAsjvvb8wgwf0DQqhEzCTFkqg';
+
+export default{
+	async fetch(request, env, ctx) {
+		const body = await request.formData();
+		// Turnstile injects a token in "cf-turnstile-response".
+		const token = body.get('cf-turnstile-response');
+		const ip = request.headers.get('CF-Connecting-IP');
+	
+		console.log('nombre: ' + body.get('nombre'))
+		console.log('email: ' + body.get('email'))
+		console.log('telefono: ' + body.get('telefono'))
+		console.log('empresa: ' + body.get('empresa'))
+		console.log('mensaje: ' + body.get('mensaje'))
+	
+		// Validate the token by calling the
+		// "/siteverify" API endpoint.
+		let formData = new FormData();
+		formData.append('secret', SECRET_KEY);
+		formData.append('response', token);
+		formData.append('remoteip', ip);
+	
+		const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+		const result = await fetch(url, {
+			body: formData,
+			method: 'POST'
+		});
+	
+		const resultHeaders = new Headers(result.headers)
+        resultHeaders.set('Access-Control-Allow-Origin', '*')
+		return new Response(result.body, {
+            headers: resultHeaders,
+            status: result.status,
+            statusText: result.statusText
+        })
+	}
+}
